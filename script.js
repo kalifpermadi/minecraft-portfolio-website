c0c93f7b4ce9cb5384293b463154ceaab6c28065
kalifpermadi <kalif.kai.permadi@gmail.com>
2023-02-12 19:40:05 +0700

    Fixed everything, still only like 90% done tho

--- script.js
+++ script.js
@@ -1,88 +1,171 @@
-// When lever is flicked
-const lever = document.getElementById("lever");
-const cover = document.getElementById("cover");
-const lamp = document.querySelectorAll(".lamp");
-const activate = document.getElementById("activate");
-const deactivate = document.getElementById("deactivate");
-const flick = document.getElementById("rotate");
-lever.onclick = function() {leverFlick()};
-var x = 0;
-function leverFlick() {
-    x++;
-    if (x % 2 == 1) {
-        activate.play();
-        lever.style.transform = "rotate(180deg)";
-        cover.style.display = "none";
-        for (let i = 0; i < lamp.length; i++) {
-            lamp[i].src = "assets/image/redstone_lamp_on.png";
-        }
+const body = document.getElementsByTagName("body");
+const doneButton = document.getElementById("done");
+const blocks = document.getElementById("blocks");
+const page = document.getElementById("page");
+const pageImage = page.querySelector("img:first-child");
+window.addEventListener("resize", function() {
+    var width = window.innerWidth;
+    if (width < "640") {
+        blocks.style.maxWidth = "384px";
+        doneButton.src = "assets/image/done_resized.png";
+        page.style.width = "300px";
+        pageImage.style.left = "4px";
     }
     else {
-        deactivate.play();
-        lever.style.transform = "rotate(0deg)";
-        cover.style.display = "block";
-        for (let i = 0; i < lamp.length; i++) {
-            lamp[i].src = "assets/image/redstone_lamp.png";
+        blocks.style.maxWidth = "640px";
+        doneButton.src = "assets/image/done.png";
+        page.style.width = "400px";
+        pageImage.style.left = "54px";
+    }
+});
+
+window.addEventListener("load", function() {
+    var width = window.innerWidth;
+    if (width < "640") {
+        blocks.style.maxWidth = "384px";
+        doneButton.src = "assets/image/done_resized.png";
+        page.style.width = "300px";
+        pageImage.style.left = "4px";
+    }
+    else {
+        blocks.style.maxWidth = "640px";
+        doneButton.src = "assets/image/done.png";
+        page.style.width = "400px";
+        pageImage.style.left = "54px";
+    }
+});
+
+
+
+const leverFlick = document.getElementById("leverFlick");
+const lamps = document.querySelectorAll(".redstoneLamp");
+var x = 0;
+function flick() {
+    x++;
+    if(x % 2 != 0) {
+        for(var i = 0; i < 4; i++) {
+            lamps[i].src = "assets/image/redstone_lamp_on.png";
+        }
+        leverFlick.style.transform = "translate(-50%, -50%) rotate(180deg)";
+        body[0].style.filter = "brightness(100%)";
+        body[0].style.backgroundImage = "url(assets/image/spruce_planks.png)";
+        return;
+    }
+    for(var i = 0; i < 4; i++) {
+        lamps[i].src = "assets/image/redstone_lamp.png";
+    }
+    leverFlick.style.transform = "translate(-50%, -50%)";
+    body[0].style.filter = "brightness(20%)";
+    body[0].style.backgroundImage = "url(assets/image/spruce_planks_dark.png)";
+}
+
+
+
+const about = document.getElementById("about");
+function showAbout() {
+    about.style.display = "block";
+    achievements.style.display = "none";
+    coolThings.style.display = "none";
+    page.style.display = "block";
+}
+
+
+const achievements = document.getElementById("achievements");
+function showAchievements() {
+    about.style.display = "none";
+    achievements.style.display = "block";
+    coolThings.style.display = "none";
+    page.style.display = "block";
+}
+
+
+const coolThings = document.getElementById("coolThings");
+function showCoolThings() {
+    about.style.display = "none";
+    achievements.style.display = "none";
+    coolThings.style.display = "block";
+    page.style.display = "block";
+}
+
+
+const contact = document.getElementById("contact");
+function showContact() {
+    about.style.display = "none";
+    achievements.style.display = "none";
+    coolThings.style.display = "none";
+    contact.style.display = "block";
+    page.style.display = "block";
+}
+
+
+function hidePage() {
+    about.style.display = "none";
+    achievements.style.display = "none";
+    coolThings.style.display = "none";
+    contact.style.display = "none";
+    page.style.display = "none";
+}
+
+
+const achievementsSlide = document.querySelectorAll(".achievementsSlide");
+var achievementsIndex = 0;
+function nextAchievements() {
+    achievementsIndex++;
+    if (achievementsIndex == achievementsSlide.length) {
+        achievementsIndex = achievementsSlide.length - 1;
+    }
+    for (var i = 0; i < achievementsSlide.length; i++) {
+        if (i == achievementsIndex % achievementsSlide.length) {
+            achievementsSlide[i].style.display = "block";
+        }
+        else {
+            achievementsSlide[i].style.display = "none";
         }
     }
-};
-
-// When "About" text is clicked
-const page = document.getElementById("page");
-const about = document.querySelector(".about");
-const aboutText = document.getElementById("about");
-aboutText.onclick = function() {showAbout()};
-function showAbout() {
-    flick.style.display = "none";
-    page.style.display = "flex";
-    about.style.display = "flex";
 }
-
-// When "Projects" text is clicked
-const work = document.querySelector(".work");
-const workText = document.getElementById("work");
-workText.onclick = function() {showWork()};
-function showWork() {
-    flick.style.display = "none";
-    page.style.display = "flex";
-    work.style.display = "flex";
-}
-
-// When "Contact" text is clicked
-const contact = document.querySelector(".contact");
-const contactText = document.getElementById("contact");
-contactText.onclick = function() {showContact()};
-function showContact() {
-    flick.style.display = "none";
-    page.style.display = "flex";
-    contact.style.display = "flex";
-}
-
-// When "Done" button is clicked
-const done = document.getElementById("done");
-done.onclick = function() {hideAbout()};
-function hideAbout() {
-    flick.style.display = "block";
-    page.style.display = "none";
-    about.style.display = "none";
-    work.style.display = "none";
-    contact.style.display = "none";
-}
-
-var pageIndex = 1;
-showPage(pageIndex);
-
-function plusPage(n){
-    showPage(pageIndex += n);
-}
-
-function showPage(n) {
-    var i;
-    var pages = document.getElementsByClassName("work-slides");
-    if (n > pages.length) {pageIndex = 1;}
-    if (n < 1) {pageIndex = pages.length;}
-    for (i = 0; i < pages.length; i++) {
-        pages[i].style.display = "none";
+function previousAchievements() {
+    achievementsIndex--;
+    if (achievementsIndex < 0) {
+        achievementsIndex = 0;
     }
-    pages[pageIndex-1].style.display = "block";
+    for (var i = 0; i < achievementsSlide.length; i++) {
+        if (i == achievementsIndex % achievementsSlide.length) {
+            achievementsSlide[i].style.display = "block";
+        }
+        else {
+            achievementsSlide[i].style.display = "none";
+        }
+    }
+}
+
+
+const coolThingsSlide = document.querySelectorAll(".coolThingsSlide");
+var coolThingsIndex = 0;
+function nextCoolThings() {
+    coolThingsIndex++;
+    if (coolThingsIndex == coolThingsSlide.length) {
+        coolThingsIndex = coolThingsSlide.length - 1;
+    }
+    for (var i = 0; i < coolThingsSlide.length; i++) {
+        if (i == coolThingsIndex % coolThingsSlide.length) {
+            coolThingsSlide[i].style.display = "block";
+        }
+        else {
+            coolThingsSlide[i].style.display = "none";
+        }
+    }
+}
+function previousCoolThings() {
+    coolThingsIndex--;
+    if (coolThingsIndex < 0) {
+        coolThingsIndex = 0;
+    }
+    for (var i = 0; i < coolThingsSlide.length; i++) {
+        if (i == coolThingsIndex % coolThingsSlide.length) {
+            coolThingsSlide[i].style.display = "block";
+        }
+        else {
+            coolThingsSlide[i].style.display = "none";
+        }
+    }
 }
